7f9148419d31c751cdff2d3ee2d554d537fb6c41
aashish rana magar <aashish@rvlife.com>
2024-02-22 16:40:49 +0545

    test cron job

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+const filename = 'myFile.json';
+const filePath = path.join(__dirname, '..', 'public/data', filename);
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async cronJob(): Promise<string> {
+    const myFile = fs.readFileSync(filePath, 'utf-8');
+    const { data } = JSON.parse(myFile.toString());
+
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({
+        data: [
+          ...data,
+          {
+            date: new Date().toLocaleString(undefined, {
+              timeZoneName: 'long',
+            }),
+          },
+        ],
+      }),
+    );
+
+    return 'Success';
+  }
+
+  async getFile(): Promise<string> {
+    const myFile = fs.readFileSync(filePath, 'utf-8');
+    return myFile.toString();
   }
 }
